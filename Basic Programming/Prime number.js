fdc41414f40e6e4d78f0cf5563a2b7b17c0bad4e
sunus <sunusukesan96@gmail.com>
2025-11-14 21:57:27 +0530

    message

--- Basic Programming/Prime number.js
+++ Basic Programming/Prime number.js
@@ -13,3 +13,18 @@ function isPrime(num) {
 console.log(isPrime(7));   
 console.log(isPrime(10));  
 console.log(isPrime(2));   
+  
+////EVEN OR ODD
+
+
+function checkEvenOdd(num) {
+    if (num % 2 === 0) {
+        return "Even";
+    } else {
+        return "Odd";
+    }
+}
+
+
+console.log(checkEvenOdd(10)); 
+console.log(checkEvenOdd(7));  
