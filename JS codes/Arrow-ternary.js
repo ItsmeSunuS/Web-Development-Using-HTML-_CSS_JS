c79c3082939010d403b1a8560a24198defb5af89
SUNU SUKESAN <48004668+ItsmeSunuS@users.noreply.github.com>
2025-11-24 21:16:19 +0530

    Update Arrow-ternary.js

--- JS codes/Arrow-ternary.js
+++ JS codes/Arrow-ternary.js
@@ -1,5 +1,6 @@
 const isEven = n => n % 2 === 0;
 const result = marks >= 35 ? "Pass" : "Fail";
 const greet = name => `Hello, ${name || "Guest"}`;
-console.log(greet("Sunu"));  // Output: "Hello, Alice"
-console.log(greet());         // Output: "Hello, Guest"
+console.log(greet("Sunu"));  // Output: "Hello, Sunu"
+console.log(greet());       
+
