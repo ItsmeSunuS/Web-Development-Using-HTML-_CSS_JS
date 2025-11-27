c677072e023c9172baff1a288ba69c52c679293d
SUNU SUKESAN <48004668+ItsmeSunuS@users.noreply.github.com>
2025-11-27 16:44:03 +0530

    Understanding Synchronous Code Execution

---
+++ JS codes/Synchronous-Excecution.js
@@ -0,0 +1,10 @@
+console.log("Task 1");
+console.log("Task 2");
+console.log("Task 3");
+
+
+//Because JavaScript executes synchronous code in order from top to bottom, the output will be:
+/*  Task 1
+    Task 2
+    Task 3
+*/
