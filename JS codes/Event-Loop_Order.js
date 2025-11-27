ce8d84aa720fff05013823bce2b1367eb4f2f89d
SUNU SUKESAN <48004668+ItsmeSunuS@users.noreply.github.com>
2025-11-27 16:53:15 +0530

    Debugging Event Loop Order

---
+++ JS codes/Event-Loop_Order.js
@@ -0,0 +1,20 @@
+console.log("Begin");
+
+setTimeout(() => {
+  console.log("Timeout Task");
+}, 0);
+
+Promise.resolve().then(() => {
+  console.log("Promise Task");
+});
+
+console.log("End");
+
+
+//Output
+
+/*Begin
+End
+Promise Task
+Timeout Task
+*/
