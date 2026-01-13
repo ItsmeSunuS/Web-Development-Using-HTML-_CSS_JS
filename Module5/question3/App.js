89816d869826c3f3fdef563002ce9fd7ef9d762f
SUNU SUKESAN <48004668+ItsmeSunuS@users.noreply.github.com>
2026-01-13 14:21:31 +0530

    Add files via upload

---
+++ Module5/question3/App.js
@@ -0,0 +1,36 @@
+import os from 'os';
+import fs from 'fs/promises';
+
+async function runApp() {
+  try {
+   
+    console.log('Free Memory:', os.freemem());
+    console.log('Total CPU Cores:', os.cpus().length);
+
+
+    // 1. Create data.txt
+    await fs.writeFile('data.txt', 'Hello World');
+    console.log('data.txt created');
+
+    // 2. Create Readme.md
+    await fs.writeFile('Readme.md', '## This is first line in Readme');
+    console.log('Readme.md created');
+
+    // 3. Read data.txt and print content
+    const data = await fs.readFile('data.txt', 'utf-8');
+    console.log('Content of data.txt:', data);
+
+    // 4. Append new line to data.txt
+    await fs.appendFile('data.txt', '\nThis is second line');
+    console.log('Content appended to data.txt');
+
+    // 5. Delete Readme.md
+    await fs.unlink('Readme.md');
+    console.log('Readme.md deleted');
+
+  } catch (error) {
+    console.error('Error:', error.message);
+  }
+}
+
+runApp();
